82538826b6e0b71388b1a045caa7f86dc47ab254
Amit Mehta <JohnWickedStine@Kunals-MacBook-Air.local>
2021-09-08 17:33:30 +0530

    second commit

--- src/customer/controller/customer.controller.ts
+++ src/customer/controller/customer.controller.ts
@@ -2,28 +2,46 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CustomerService } from '../service/customer.service';
 import { CustomerEntity } from '../model/customer.entity';
 import { Customer } from '../model/customer.interface';
+import { Repository } from 'typeorm';
+import { InvoiceService } from '../../invoice/service/invoice.service';
 
 @Controller('customer')
 export class CustomerController {
 
-    constructor(private customerService: CustomerService) { }
-
+    constructor(private customerService: CustomerService,
+        private invoiceService: InvoiceService) { }
 
     // @Post()
-    // createCustomer(@Body() customer: Promise<CustomerEntity>{
+    // createCustomer(customer: Promise<CustomerEntity>{```
     // return this.customerRepository.save(details);    
     // }
 
+    @Post()
+    createCustomer(
+     name: string,
+     email: string,
+     phone: number,
+     address: string,
+  ): Promise<CustomerEntity> {
+    return this.customerService.create({ name, email, phone, address })
+  }
+
     // @Get()
     // findAll(): Promise<CustomerEntity>{
-    // return thix.CustomerRepository.find();  
-    // }    
+    // return this.customerService.findAll();  
+    // } 
 
 
     // @Get()
     // findOne(id: string): Promise<CustomerEntity> {
     // return this.customerRepository.findOne(id);
     // }
-
+    
+    // @Get('/:id')
+    // invoices(@Parent() customer) {
+    //     const { id } = customer;
+    //     console.log(customer);
+    //     return this.invoiceService.findByCustomer(id);
+    // }
 
 }
--- src/invoice/controller/invoice.controller.ts
+++ src/invoice/controller/invoice.controller.ts
@@ -1,53 +1,58 @@
 import { Controller, Get, Post } from '@nestjs/common';
-import { string } from 'yargs';
-import { Observable, of } from 'rxjs';
+// import { string } from 'yargs';
+// import { Observable, of } from 'rxjs';
 import { InvoiceService } from '../service/invoice.service';
 import { InvoiceEntity } from '../model/invoice.entity';
+import { Invoice } from '../model/invoice.interface';
+import { Repository } from 'typeorm'
+import { CustomerService } from '../../customer/service/customer.service';
 
 @Controller('invoice')
 export class InvoiceController {
 
-    constructor(private invoiceService: InvoiceService) { }
+    constructor(private invoiceService: InvoiceService,
+        private customerService: CustomerService,
+        private invoiceRepository: Repository<InvoiceEntity>  ) { }
 
 
-    // @Get()
-    // findAll(): Promise<InvoiceEntity[]> {
-    // return this.invoiceService.findAll();
-    // }
+    @Get()
+    findAll(): Promise<InvoiceEntity[]> {
+    return this.invoiceService.findAll();
+    }
 
-    // @Get()
-    // findByCustomer(id: string): Promise<InvoiceEntity[]>{
-    // return this.invoiceRepository.createQueryBuilder("invoice")
-    // .where("invoice.customer = :id", { id })
-    // .getMany();
-    //  }
+    @Get()
+    findByCustomer(id: string): Promise<InvoiceEntity[]>{
+    return this.invoiceRepository.createQueryBuilder("invoice")
+    .where("invoice.customer = :id", { id })
+    .getMany();
+     }
 
-    // @Get()
-    // findOne(id: string) : Promise <InvoiceEntity>{
-    // return this.invoiceRepository.findOne(id);    
-    // }
+    @Get()
+    findOne(id: string) : Promise <InvoiceEntity>{
+    return this.invoiceRepository.findOne(id);    
+    }
 
-    // @Post()
-    // create(invoice: Invoice): Promise<InvoiceEntity> {
-    //     const customer = await this.customerService.findOne(invoice.customer);
-    //     const subTotal = invoice.items.reduce((acc, curr) => {
-    //       return acc + Number((curr.rate * curr.quantity).toFixed(2))
-    //     }, 0)
+    @Post()
+    create(invoice: Invoice): Promise<InvoiceEntity> {
+        const customer = this.customerService.findOne(invoice.customer);
+        const subTotal = invoice.items.reduce((acc, curr) => {
+          return acc + Number((curr.rate * curr.quantity).toFixed(2))
+        }, 0)
 
     
-    //     const taxAmount = subTotal * Number((invoice.taxRate / 100).toFixed(2));
-    //     const total = subTotal + taxAmount;
-    //     const outstandingBalance = total;
-    //     return this.invoiceRepository.save({
-    //       ...invoice,
-    //       customer,
-    //       subTotal,
-    //       taxAmount,
-    //       total,
-    //       outstandingBalance
-    //     } as any);
+        const taxAmount = subTotal * Number((invoice.taxRate / 100).toFixed(2));
+        const total = subTotal + taxAmount;
+        const outstandingBalance = total;
+        return this.invoiceRepository.save({
+          ...invoice,
+          customer,
+          subTotal,
+          taxAmount,
+          total,
+          outstandingBalance
+        } as any);
     
-    //   }
+      }
 
     
 }
